566a9d9de9bf7519b2fd701b7c43972dc4ae7ae8
SmaranSatapathy <smaransatapathy31@gmail.com>
2025-01-09 10:39:24 +0530

    Assignment 4 Updated.

--- Assignment4/q3.c
+++ Assignment4/q3.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 #include <math.h>
 
+void square_root_calculator(int N,double *LG,double *NG)
+{
+    while(1)
+    {
+        *NG=0.5*(*LG+(N/ *LG));
+         if(fabs(*NG-*LG)>0.005)
+         *LG=*NG;
+         else
+         break;
+    }
+}
+
 int main()
 {
-    double NG=0,LG=1.0;
     int N;
-    printf("Enter number:");
-    scanf("%d",&N);
+    double LG=1.0,NG=100;
 
-  while(1)
-  {
-    NG=0.5*(LG+N/LG);
-    if(fabs(NG-LG)>0.005)
-    LG=NG;
-    else
-    break;
-  }
-    printf("Answer is: %lf",NG);
+    printf("Enter number: ");
+    scanf("%d",&N);
+    square_root_calculator(N,&LG,&NG);
+    printf("Square root of %d is: %lf",N,NG);
 }
